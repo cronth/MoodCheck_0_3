38977b6c3b92e5f14c416281dd263f51ff8bab2b
cronth <48060258+cronth@users.noreply.github.com>
2020-09-07 15:23:31 +0200

    X Layout grob fertig. X Toast mit resultSet und Zeit, wenn send gedrückt wird O Speichern von Auswahl und Zeit in DB O Toast mit Datenbankeintrag O 'send' schließt LoggerModel (und öffnet neues? schließt App?)
    Backlog:
    Buttons gleich groß,
    neuere Klasse für Zeit/Datumausgabe

--- app/src/main/java/de/sialos/moodcheck_0_3/LoggerModel.java
+++ app/src/main/java/de/sialos/moodcheck_0_3/LoggerModel.java
@@ -1,19 +1,27 @@
 package de.sialos.moodcheck_0_3;
 
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class LoggerModel {
 
-    private int timeDate;
-    private int resultSet;
+    private final int MAX_VALUE_OF_RESULT = 2;
+
     Date date = new Date();
+    // SimpleDateFormat ist veraltet und "troublesome", in NÄCHSTER Version etwas neueres benutzen!
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    String readableDateTime = sdf.format(date);
+    private int timeDate;
+    private int result;
+    private int resultSet = 0;
 
+    public String getReadableDateTime() {
+        return readableDateTime;
+    }
 
-    public LoggerModel(int resultSet) {
-        this.resultSet = resultSet;
+    public LoggerModel() {
         this.timeDate = (int) date.getTime();
-
     }
 
     public int getTimeDate() {
@@ -22,6 +30,18 @@ public class LoggerModel {
 
     public int getResultSet() {
         return resultSet;
+    }
+
+    public void updateResultSet(int result) {
+        if (result > 0 && result <= MAX_VALUE_OF_RESULT) {
+            if (resultSet == 0) {
+                resultSet = result;
+            } else {
+                resultSet = resultSet * 10 + result;
+            }
+        } else {
+            // TODO Fehlerausgabe
+        }
     }
 
 }
--- app/src/main/java/de/sialos/moodcheck_0_3/MainActivity.java
+++ app/src/main/java/de/sialos/moodcheck_0_3/MainActivity.java
@@ -3,12 +3,60 @@ package de.sialos.moodcheck_0_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    LoggerModel loggerModel;
+
+    Button btn_1; // "schlecht"
+    Button btn_2; // "gut"
+    Button btn_3; // "send"
+
+    int result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loggerModel = new LoggerModel();
+        btn_1 = findViewById(R.id.btn_1);
+        btn_2 = findViewById(R.id.btn_2);
+        btn_3 = findViewById(R.id.btn_3);
+
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "1 gedrückt", Toast.LENGTH_SHORT).show();
+                result = 1;
+            }
+        });
+
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "2 gedrückt", Toast.LENGTH_SHORT).show();
+                result = 2;
+            }
+        });
+
+        btn_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (result != 0) {
+                    loggerModel.updateResultSet(result);
+                    Toast.makeText(MainActivity.this,
+                            "ResultSet is " + loggerModel.getResultSet()
+                                    + "\nTime is " + loggerModel.getReadableDateTime()
+                            , Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, getString(R.string.str_choose_answer_first), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
